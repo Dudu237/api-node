caf7b9a8d1e523c571a2603f73f0c3301ce6ea51
Thiago Nobrega <thiago.fnobrega@senacsp.edu.br>
2024-04-11 10:37:16 -0300

    chore: create route examples

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const PORT = 3000;
 
 const rotas = {
   "/": "Curso de Node.js",
+  "/cachorros": "Entrei na rota cachorros",
+  "/racas": "Entrei na rota raças"
 };
 
 const server = http.createServer((req, res) => {
