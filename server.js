cc8b52f47bc8f2cb438d3174169ddbf64e214311
Thiago Nobrega <thiago.fnobrega@senacsp.edu.br>
2024-04-11 11:56:11 -0300

    chore: crud

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-import http from "http";
+// import http from "http";
+import app from "./src/app.js";
 
 const PORT = 3000;
 
-const rotas = {
-  "/": "Curso de Node.js",
-  "/cachorros": "Entrei na rota cachorros",
-  "/racas": "Entrei na rota raças"
-};
+// const rotas = {
+//   "/": "Curso de Node.js",
+//   "/cachorros": "Entrei na rota cachorros",
+//   "/racas": "Entrei na rota raças"
+// };
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-type": "text/plain" });
-  res.end(rotas[req.url]);
-});
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { "Content-type": "text/plain" });
+//   res.end(rotas[req.url]);
+// });
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log("Servidor ok");
 });
