65c5974a71f60ed87d7f73fef74fd900c36de519
Thiago Nobrega <thiago.fnobrega@senacsp.edu.br>
2024-04-18 09:36:45 -0300

    feat: create model

---
+++ src/models/Cachorro.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const cachorroSchema = new mongoose.Schema(
+  {
+    id: { type: mongoose.Schema.Types.ObjectId },
+    nome: { type: String, required: true },
+    raca: { type: String },
+    idade: { type: Number },
+    vacinado: { type: Boolean },
+  },
+  { versionKey: false }
+);
+
+const cachorro = mongoose.model("cachorros", cachorroSchema);
+
+export default cachorro;
