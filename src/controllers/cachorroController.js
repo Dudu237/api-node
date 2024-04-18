809cb2c4c3ba78a54cd2aa6054a47f86d66c3fca
Thiago Nobrega <thiago.fnobrega@senacsp.edu.br>
2024-04-18 11:51:09 -0300

    chore: criar cadastrar cachorro

--- src/controllers/cachorroController.js
+++ src/controllers/cachorroController.js
@@ -6,6 +6,20 @@ class CachorroController {
     // get all
     res.status(200).json(listaCachorros);
   }
+
+  static async cadastrarCachorro(req, res) {
+    try {
+      const novoCachorro = await cachorro.create(req.body);
+      res.status(201).json({
+        message: "Cachorro inserido com sucesso.",
+        cachorro: novoCachorro,
+      });
+    } catch (erro) {
+      res.status(500).json({
+        message: `${erro.message} - Falha ao cadastrar cachorro `,
+      });
+    }
+  }
 }
 
 export default CachorroController;
